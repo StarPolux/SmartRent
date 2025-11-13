82f96e56c88b3e1b2bc5e3b7792c8127b97dcaf5
Polux <deiby_juan@hotmail.com>
2025-11-13 15:34:48 -0500

    Tercera muestra

--- js/index.js
+++ js/index.js
@@ -1,15 +1,60 @@
-var modal = document.getElementById("loginModal");
-var btn = document.querySelector(".btn-login");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    modal.style.display = "block";
+
+var loginModal = document.getElementById("loginModal");
+var registerModal = document.getElementById("registerModal");
+var successLoginModal = document.getElementById("successLoginModal");
+var successRegisterModal = document.getElementById("successRegisterModal");
+var btnLogin = document.querySelector(".btn-login");
+var closeLogin = document.querySelector(".close");
+var closeRegister = document.querySelector(".close-register");
+btnLogin.onclick = function() {
+    loginModal.style.display = "block";
 }
-span.onclick = function() {
-    modal.style.display = "none";
+
+closeLogin.onclick = function() {
+    loginModal.style.display = "none";
+}
+
+closeRegister.onclick = function() {
+    registerModal.style.display = "none";
+}
+
+document.querySelector(".register-link a").onclick = function(e) {
+    e.preventDefault();
+    loginModal.style.display = "none";
+    registerModal.style.display = "block";
+}
+
+document.querySelector("#loginModal form").onsubmit = function(e) {
+    e.preventDefault();
+    loginModal.style.display = "none";
+    successLoginModal.style.display = "block";
+}
+
+document.getElementById("registerForm").onsubmit = function(e) {
+    e.preventDefault();
+    registerModal.style.display = "none";
+    successRegisterModal.style.display = "block";
+}
+
+function closeSuccessLogin() {
+    successLoginModal.style.display = "none";
+}
+
+function closeSuccessRegister() {
+    successRegisterModal.style.display = "none";
 }
 
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == loginModal) {
+        loginModal.style.display = "none";
+    }
+    if (event.target == registerModal) {
+        registerModal.style.display = "none";
+    }
+    if (event.target == successLoginModal) {
+        successLoginModal.style.display = "none";
+    }
+    if (event.target == successRegisterModal) {
+        successRegisterModal.style.display = "none";
     }
 }
